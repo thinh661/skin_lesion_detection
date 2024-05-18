9754cab89a7b343a5694317239b370613c346bfc
thinh661 <nguyencongthinh_t66@hus.edu.vn>
2024-05-18 21:28:47 +0700

    update GUI

--- demo/src/Interface.js
+++ demo/src/Interface.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../src/style/style.css';
 
@@ -6,6 +6,8 @@ function Interface() {
   const [result, setResult] = useState('');
   const [message, setMessage] = useState(''); // Thêm state để lưu trữ thông báo trả về từ API
   const [image, setImage] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [manualNav, setManualNav] = useState(false);
 
   const diseaseNames = [
     "Actinic Keratosis",
@@ -16,21 +18,38 @@ function Interface() {
     "Nevus",
     "Vascular Lesion"
   ];
-  
+
+  const images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4bW-zOzOjbTwNXEvguVe1AawtNlG8Fqe1XJ7mmBKSsw&s",
+    "https://www.researchgate.net/publication/259714110/figure/fig3/AS:667714678964235@1536206993911/Examples-of-skin-lesion-images-from-the-different-classes-used-in-this-work.jpg",
+    "https://lazaderm.com/wp-content/uploads/2020/01/LZ_PigmentedLesions-1-e1583847793564.jpg",
+    "https://cdn.medizzy.com/1wLDOEemTOHuWCwuqcIcSllX0_I=/720x505/img/posts/77e40fbc-6dc7-4f25-a40f-bc6dc7ff25f7"
+  ];
+
+  useEffect(() => {
+    if (!manualNav) {
+      const interval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [manualNav, images.length]);
+
   const handleImageUpload = async (event) => {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
-    setImage(URL.createObjectURL(file)); // Display the uploaded image
-  
+    setImage(URL.createObjectURL(file));
+
     try {
       const response = await axios.post('http://localhost:5000/process_image', formData);
       const predictedClass = response.data.message[0];
       const predictedProbability = response.data.message[1] * 100;
 
-      setResult(predictedClass); // Assuming response.data.message contains the predicted_class
+      setResult(predictedClass);
       if (!isNaN(predictedProbability)) {
-        setMessage(`The predicted disease is ${diseaseNames[predictedClass]} with ${(predictedProbability).toFixed(2)}% probability.`);
+        setMessage(`Probability : ${(predictedProbability).toFixed(2)}% .`);
       } else {
         setMessage(`The predicted disease is ${diseaseNames[predictedClass]}.`);
       }
@@ -38,30 +57,57 @@ function Interface() {
       console.error(error);
     }
   };
-  
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setManualNav(true);
+    resetManualNav();
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setManualNav(true);
+    resetManualNav();
+  };
+
+  const resetManualNav = () => {
+    setTimeout(() => {
+      setManualNav(false);
+    }, 3000);
+  };
 
   return (
     <div className="container">
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Skin Legion Detection</title>
-      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
+      <title>Skin Lesion Detection</title>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Montserrat:wght@700&display=swap" rel="stylesheet" />
       <div className="header">
-        <h2>Skin Lesion Detection</h2>
-        <img className="header-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4bW-zOzOjbTwNXEvguVe1AawtNlG8Fqe1XJ7mmBKSsw&s" alt="Header" />
+        <h1 className="title">SKIN LESION DETECTION</h1>
+        <div className="header-image-wrapper">
+          <button className="nav-button left" onClick={prevImage}>‹</button>
+          <img className="header-image" src={images[currentImageIndex]} alt="Header" />
+          <button className="nav-button right" onClick={nextImage}>›</button>
+          <div className="dots-container">
+            {images.map((_, index) => (
+              <span key={index} className={`dot ${index === currentImageIndex ? 'active' : ''}`} onClick={() => setCurrentImageIndex(index)}></span>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="upload-file-image">
-        <h4>Upload an Image</h4>
-        <input type="file" onChange={handleImageUpload} />
+        <div className="upload-box">
+          <h4 className="upload-title">Upload Image</h4>
+          <input type="file" onChange={handleImageUpload} className="upload-input" />
+        </div>
       </div>
-      <hr />
       <div className="prediction-div">
         {image && <img className="uploaded-image" src={image} alt="Uploaded" />}
         {result !== '' && (
-          <h3 className="predicted-disease">
-            The predicted disease is <span>{diseaseNames[result]}</span>.
-          </h3>
-        )}
+        <h3 className="predicted-disease">
+          The predicted disease is <span className="highlight">{diseaseNames[result]}</span>.
+        </h3>
+      )}
         {message !== '' && <p className="prediction-message">{message}</p>} {/* Hiển thị thông báo trả về từ API */}
       </div>
     </div>
